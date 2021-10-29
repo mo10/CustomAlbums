3d288093d7b6ed90f0d9bbcc781df1eb3982ca0a
某10 <0w0@0w0.pw>
2021-10-30 02:10:53 +0800

    Fix 2021.10.29 update /musedash/v1/music_tag

--- CustomAlbums/Patch/WebApiPatch.cs
+++ CustomAlbums/Patch/WebApiPatch.cs
@@ -16,7 +16,7 @@ namespace CustomAlbums.Patch
             // ServerManager.SendToUrl
             var sendToUrl = AccessTools.Method(typeof(ServerManager), "SendToUrl");
             var sendToUrlPrefix = AccessTools.Method(typeof(WebApiPatch), "SendToUrlPrefix");
-            harmony.Patch(sendToUrl, prefix:new HarmonyMethod(sendToUrlPrefix));
+            harmony.Patch(sendToUrl, prefix: new HarmonyMethod(sendToUrlPrefix));
         }
         public static bool SendToUrlPrefix(ref string url, ref string method, ref Dictionary<string, object> datas,
             ref Action<JObject> callback,
@@ -29,8 +29,33 @@ namespace CustomAlbums.Patch
             if ("/musedash/v1/music_tag" == url)
             {
                 var oldCallback = callback;
-                callback = delegate(JObject jObject){
+                callback = delegate (JObject jObject) {
                     var jArray = (JArray)jObject["music_tag_list"];
+
+
+                    foreach (JToken obj in jArray)
+                    {
+                        if ((string)obj["object_id"] != "61557604a75ed5015c2e439a")
+                            continue;
+                        ModLogger.Debug("Fucked");
+                        JArray new_music_list = new JArray();
+                        foreach (var uid in obj["music_list"])
+                        {
+                            string text = (string)uid;
+                            if (text == "45-3" ||
+                                text == "45-4" ||
+                                text == "45-5")
+                                continue;
+
+                            if (text.StartsWith("45-"))
+                                new_music_list.Add("44-" + text.RemoveFromStart("45-"));
+                            else
+                                new_music_list.Add(uid);
+                        }
+                        obj["music_list"] = new_music_list;
+                    }
+
+
                     jArray.Add(JObject.FromObject(new
                     {
                         object_id = "3d2be24f837b2ec1e5e119bb",
