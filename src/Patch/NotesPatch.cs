fbc125985285f8e89e9f071ee45c2c24b1d0578b
Colman Davenport <colmandavenport@gmail.com>
2022-02-18 17:42:25 -0500

    Add DJMax scene switch note defintion

---
+++ src/Patch/NotesPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Assets.Scripts.GameCore.Managers;
+using GameLogic;
+
+namespace CustomAlbums.Patch
+{
+    /// <summary>
+    /// Adds notes to the NoteDatas.
+    /// </summary>
+    [HarmonyPatch(typeof(NoteDataMananger), "Init")]
+    internal static class NotesPatch
+    {
+        private static void Postfix(NoteDataMananger __instance) {
+            // Add DJMax scene switch
+            __instance.NoteDatas.Add(new NoteConfigData {
+                ibms_id = "1W",
+
+                m_BmsUid = PeroPeroGames.GlobalDefines.BmsNodeUid.ToggleScene10,
+                id = (__instance.NoteDatas.Count + 1).ToString(),
+                des = "DJMax Scene Switch",
+                prefab_name = "000401",
+                uid = "000409",
+                noteUid = 409,
+                isShowPlayEffect = true,
+                scene = "0",
+                boss_action = "0",
+                key_audio = "0",
+                effect = "0"
+            });
+        }
+    }
+}
