0a40eda9cd7633f11c4abbc502c9b8ed51227282
Colman Davenport <colmandavenport@gmail.com>
2022-03-04 12:45:56 -0500

    Make PPG not gay again

---
+++ src/Patch/PeroPeroGaysPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace CustomAlbums.Patch
+{
+	/// <summary>
+	/// Stops PPG from being gay
+	/// 
+	/// Actual function: Disables the check on number of files within game directories for the purpose of Steam API initialization.
+	/// Fixes an issue where any excess files in the base game directory would cause it to refuse to recognize the DLC.
+	/// </summary>
+	[HarmonyPatch(typeof(SteamManager), nameof(SteamManager.DoSomething2))]
+	internal static class PeroPeroGaysPatch
+	{
+		private static bool Prefix(ref bool __result) {
+			__result = true;
+			return false;
+		}
+	}
+}
