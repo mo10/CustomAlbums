48fb3557115f1b24e8b3b46e05159f8f81ec78ce
Colman Davenport <colmandavenport@gmail.com>
2022-04-06 13:40:42 -0400

    remove dosomething2 patch

--- src/Patch/PeroPeroGaysPatch.cs
+++ src/Patch/PeroPeroGaysPatch.cs
@@ -7,13 +7,15 @@ namespace CustomAlbums.Patch
 	/// 
 	/// Actual function: Disables the check on number of files within game directories for the purpose of Steam API initialization.
 	/// Fixes an issue where any excess files in the base game directory would cause it to refuse to recognize the DLC.
+	/// 
+	/// Patch removed on 4/6/2022, when PPG removed this from the game. Archived in case they pull this again.
 	/// </summary>
-	[HarmonyPatch(typeof(SteamManager), nameof(SteamManager.DoSomething2))]
+	/*[HarmonyPatch(typeof(SteamManager), nameof(SteamManager.DoSomething2))]
 	internal static class PeroPeroGaysPatch
 	{
 		private static bool Prefix(ref bool __result) {
 			__result = true;
 			return false;
 		}
-	}
+	}*/
 }
